c68d97e8376f70fcd47817d18e88cc2e59d77677
lucarandecker <lr050@hdm-stuttgart.de>
2019-04-18 11:29:25 +0200

    Setup Article & AritcleMappeclass

---
+++ Project_19/src/de/hdm/softwarepraktikum/shared/Article.java
@@ -0,0 +1,11 @@
+package de.hdm.softwarepraktikum.shared;
+
+/*
+ * Neben get und set Name/Unit wurden keine weiteren Methoden deklariert, da die Articleklasse von ihrer
+ * Superklasse BusinessObject erbt, in der die nötigen Methoden bereits implementiert sind.
+ */
+
+public class Article {
+
+	private static final long serialVersionUID = 1L;
+}
