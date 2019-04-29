14fabf48f46d163cc9dfa9a7fb740f9ee4187a3c
vincentwengert <vincent.wengert@web.de>
2019-04-29 11:17:35 +0200

    added header report

---
+++ Project_19/src/de/hdm/softwarepraktikum/client/gui/report/Header.java
@@ -0,0 +1,138 @@
+package de.hdm.softwarepraktikum.client.gui.report;
+
+/**
+ * Die <code>Header</code>-Klasse ist der Kopfbereich des Kontaktverwaltungstools. 
+ * Der Header wird über alle Seiten des Tools gleich angezeigt. 
+ * 
+ * @author Tobias Moser, Vincent Wengert
+ * @version 1.0
+ */
+
+public class Header extends HorizontalPanel {
+				
+		private User u = CurrentReportUser.getUser();
+		
+		private HorizontalPanel homeButtonPanel = new HorizontalPanel();
+		private VerticalPanel userPanel = new VerticalPanel();
+		private HorizontalPanel topPanel = new HorizontalPanel();
+		
+		private Button logoutButton = new Button("Logout");
+		private Button editorButton = new Button ("Editor");
+		private Button reportGeneratorButton = new Button("Reportgenerator");
+		private Image logo = new Image ();
+		private Label userLabel = new Label();
+		private Anchor contactEditorLink = new Anchor("Kontakt-Editor");
+		
+		
+		/**
+		 * Im Konstruktor dieser Klasse werden die Buttons in die Panels
+		 * und zu den Buttons die ClickHandler hinzugefügt.
+		 */
+		public Header() {
+			
+			this.add(logo);
+			logo.setUrl("images/logoSpaceContacts.png");
+			
+			userPanel.add(logoutButton);
+			userLabel.setText(u.getGmailAddress());
+			userPanel.add(userLabel);
+			topPanel.add(homeButtonPanel);
+			topPanel.add(userPanel);
+			topPanel.add(userPanel);
+
+			this.add(topPanel);
+			
+			logoutButton.addClickHandler(new LogoutClickHandler());
+			editorButton.addClickHandler(new ContactEditorClickHandler());
+			reportGeneratorButton.addClickHandler(new ReportGeneratorClickHandler() );
+		}
+		
+		
+		/**
+		 * In dieser Methode werden die Desings der Buttons festgelegt. Auch
+		 * die Kontakt-Editor und ReportGenerator-Buttons werden zum Kopfbereich
+		 * des Kontaktverwaltungstools hinzugefügt. 
+		 */
+		public void onLoad() {
+			
+			this.setStylePrimaryName("headerPanel");
+			this.setHeight("15vh");
+			this.setWidth("100%");
+			
+			homeButtonPanel.add(editorButton);
+			homeButtonPanel.add(reportGeneratorButton);
+			homeButtonPanel.setWidth("26vw");
+
+			editorButton.setWidth("15vw");
+			editorButton.setHeight("15vh");
+		
+			reportGeneratorButton.setWidth("15vw");
+			reportGeneratorButton.setHeight("15vh");
+			
+			logoutButton.setHeight("40px");
+			
+			homeButtonPanel.setStylePrimaryName("homeButtonPanel");
+			logoutButton.setStylePrimaryName("logoutButton");
+			editorButton.setStylePrimaryName("editorButton");
+			reportGeneratorButton.setStylePrimaryName("reportGeneratorButton");
+			userPanel.setStylePrimaryName("userPanel");
+			
+			userPanel.setCellHorizontalAlignment(logoutButton, ALIGN_CENTER);
+			homeButtonPanel.setCellHorizontalAlignment(editorButton, ALIGN_LEFT);
+			homeButtonPanel.setCellHorizontalAlignment(reportGeneratorButton, ALIGN_RIGHT);
+			topPanel.setCellHorizontalAlignment(userPanel, ALIGN_RIGHT);
+			topPanel.setCellVerticalAlignment(userPanel,ALIGN_MIDDLE);
+			this.setCellHorizontalAlignment(topPanel, ALIGN_RIGHT);
+			this.setCellVerticalAlignment(topPanel, ALIGN_MIDDLE);
+			this.setCellVerticalAlignment(logo, ALIGN_MIDDLE);
+			
+		}
+		
+		
+		/**
+		 * Durch ein Klick auf den Kontakt-Editor-Button wird man 
+		 * auf die Kontakt-Editor-Seite weitergeleitet.
+		 */
+		private class ContactEditorClickHandler implements ClickHandler {
+
+			@Override
+			public void onClick(ClickEvent event) {
+			
+				contactEditorLink.setHref(GWT.getHostPageBaseURL() + "IT_Projekt_SS18.html");
+				Window.open(contactEditorLink.getHref(), "_self", "");
+				
+			}
+		}
+		
+		
+		/**
+		 * Durch ein Klick auf den Report-Generator-Editor-Button wird die Report-Generator-Seite
+		 * aktualisiert.
+		 */
+		private class ReportGeneratorClickHandler implements ClickHandler {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				Window.Location.reload();
+				
+			}
+		}
+		
+		
+		/**
+		 * Durch ein Klick auf den Logout-Button wird der User auf die
+		 * Begrüßungsseite weitergeleitet
+		 */
+		private class LogoutClickHandler  implements ClickHandler {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				u.setLogoutUrl(u.getLogoutUrl());
+				Window.open(u.getLogoutUrl(), "_self", "");
+			}	
+		}
+	}
+
+
