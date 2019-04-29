239db96bc77189bdb52fac09aeae305d0f08aa6b
BrunoHerceg <bh054@hdm-stuttgart.de>
2019-04-29 12:53:29 +0200

    Group

---
+++ Project_19/src/de/hdm/softwarepraktikum/server/db/GroupMapper.java
@@ -0,0 +1,162 @@
+package de.hdm.softwarepraktikum.server.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import de.hdm.softwarepraktikum.shared.bo.Group;
+
+import java.util.ArrayList;
+
+public class GroupMapper {
+
+	/**
+	   * Die Klasse GroupMapper wird nur einmal instantiiert. Man spricht hierbei
+	   * von einem sogenannten <b>Singleton</b>.
+	   * <p>
+	   * Diese Variable ist durch den Bezeichner <code>static</code> nur einmal für
+	   * sämtliche eventuellen Instanzen dieser Klasse vorhanden. Sie speichert die
+	   * einzige Instanz dieser Klasse.
+	   * 
+	   * @author Peter Thies
+	   */
+	
+	private static GroupMapper groupMapper = null;
+	
+	/**
+	   * Geschützter Konstruktor - verhindert die Möglichkeit, mit <code>new</code>
+	   * neue Instanzen dieser Klasse zu erzeugen.
+	   */
+	
+	protected GroupMapper() {
+		
+	}
+	
+	/*
+	 * Einhaltung der Singleton Eigenschaft des Mappers.
+	 */
+	
+	public static GroupMapper groupMapper() {
+		
+		if(groupMapper == null) {
+			groupMapper = new GroupMapper();
+		}
+		
+		return groupMapper;
+	}
+	
+	
+	/*
+	 * Einkäufergruppe anhand ihrer Id suchen.
+	 */
+	
+	public Group findById(int bo_id) {
+		
+		//Herstellung einer Verbindung zur DB-Connection
+				Connection con =DBConnection.connection();
+				
+		try {
+			//leeres SQL-Statement (JDBC) anlegen
+			Statement stmt = con.createStatement();
+			
+			//Statement ausfüllen und als Query an die DB schicken
+			ResultSet rs = stmt.executeQuery("Select bo_id, name, user FROM person" + "WHERE bo_id= " + bo_id);
+			
+			/*
+		     * Da id Primärschlüssel ist, kann max. nur ein Tupel zurückgegeben
+		     * werden. Prüfe, ob ein Ergebnis vorliegt.
+		     */
+			if (rs.next()) {
+				//Ergebnis-Tupel in Objekt umwandeln
+				Group g = new Group();
+				g.setBo_id(rs.getInt("bo_id"));
+				g.setName(rs.getString("name"));
+				g.setUser(rs.getPerson("user"));
+				
+				return g;		
+	}
+		}catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	return null;
+	}
+	
+	public Group insert(Group g) {
+		Connection con = DBConnection.connection();
+		
+		try {
+			Statement stmt = con.createStatement();
+			
+			/*
+			 * Zunächst schauen wir nach, welches der momentan höchste
+			 * Primärschlüsselwert ist.
+			 */
+			ResultSet rs = stmt.executeQuery("SELECT MAX(group_id) AS maxgroup_id " + "FROM group ");
+			
+			// Wenn wir etwas zurückerhalten, kann dies nur einzeilig sein
+			if (rs.next()) {
+			/*
+			 * g erhält den bisher maximalen, nun um 1 inkrementierten
+			 * Primärschlüssel.
+			 */
+			g.setBo_id(rs.getInt("maxgroup_id") + 1);
+					
+			stmt = con.createStatement();
+							
+			// Jetzt erst erfolgt die tatsächliche Einfügeoperation
+			stmt.executeUpdate("INSERT INTO groups (bo_id, name, user) " + "VALUES (" + g.getBo_Id() + ",'"
+					+ g.getName() + "','" + g.getPerson() + "')");
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+		
+		/*
+		 * Rückgabe, der evtl. korrigierten Group.
+		 */
+		return g;
+	}
+	
+	/**
+	 * Wiederholtes Schreiben eines Objekts in die Datenbank.
+	 * 
+	 * @param g
+	 *            das Objekt, das in die DB geschrieben werden soll
+	 * @return das als Parameter übergebene Objekt
+	 */
+	
+	public Group update(Group g) {
+		Connection con = DBConnection.connection();
+		
+		try {
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("UPDATE groups " + "SET name=\"" + g.getName() + "\", " + "user=\""
+					+ g.getPerson() + "\" " + "WHERE group_id=" + g.getBo_id());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Um Analogie zu insert(Group g) zu wahren, wird g zurückgegeben
+				return g;
+	}
+	
+	//Löschung einer Gruppe
+	public void delete(Group g) {
+		Connection con = DBConnection.connection();
+		
+		try {
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("DELETE FROM groups " + "WHERE group_id=" + g.getBo_id());
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
