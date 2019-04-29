9c0181620965da20141bc1d7e879293ba720106d
lucarandecker <lr050@hdm-stuttgart.de>
2019-04-29 13:38:31 +0200

    Setup first Classes Report Generator
    Report
    SimpleReport
    ItemsByPersonReport
    ItemsByGroupReport

--- Project_19/src/de/hdm/softwarepraktikum/shared/report/HTMLReportWriter.java
+++ Project_19/src/de/hdm/softwarepraktikum/shared/report/HTMLReportWriter.java
@@ -3,18 +3,5 @@ package de.hdm.softwarepraktikum.shared.report;
 import java.util.ArrayList;
 
 public class HTMLReportWriter extends ReportWriter {
-	
-	/**
-	 *  Der <code>ReportWriter</code> formatiert Reports mittels HTML. Das im
-	 * Zielformat vorliegende Ergebnis wird in der Variable reportText
-	 * abgelegt und kann nach Aufruf der entsprechenden Prozessierungsmethode mit
-	 * getReportText() ausgelesen werden.
-	 * 
-	 * @author Luca Randecker
-	 * @version 1.0
-	 */
-	
-	private static final long serialVersionUID = 1L;
-	private String reportText;
 
 }
