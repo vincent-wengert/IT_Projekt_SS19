fcaa50e145501ef5da076639ec4cba5bf5c070c3
lucarandecker <lr050@hdm-stuttgart.de>
2019-05-13 13:07:50 +0200

    ReportGenerator Changes
    Added Interface
    Added Methods to Impl

--- Project_19/src/de/hdm/softwarepraktikum/server/report/ReportGeneratorImpl.java
+++ Project_19/src/de/hdm/softwarepraktikum/server/report/ReportGeneratorImpl.java
@@ -1,9 +1,98 @@
 package de.hdm.softwarepraktikum.server.report;
 
-public class ReportGeneratorImpl {
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Map;
 
-	public ReportGeneratorImpl() {
+import de.hdm.softwarepraktikum.server.ShoppingListAdministrationImpl;
+import de.hdm.softwarepraktikum.shared.ShoppingListAdministration;
+import de.hdm.softwarepraktikum.shared.ReportGenerator;
+import de.hdm.softwarepraktikum.shared.bo.Person;
+import de.hdm.softwarepraktikum.shared.bo.Group;
+import de.hdm.softwarepraktikum.shared.bo.Item;
+import de.hdm.softwarepraktikum.shared.bo.Responsibility;
+import de.hdm.softwarepraktikum.shared.bo.ShoppingList;
+import de.hdm.softwarepraktikum.shared.bo.Store;
+import de.hdm.softwarepraktikum.shared.report.ItemsByGroupReport;
+import de.hdm.softwarepraktikum.shared.report.ItemsByPersonReport;
+import de.hdm.softwarepraktikum.shared.report.Column;
+import de.hdm.softwarepraktikum.shared.report.SubColumn;
+import de.hdm.softwarepraktikum.shared.report.Row;
+
+/**
+ * Die Klasse <code>ReportGeneratorImpl</code> implementiert das Interface
+ * ReportGenerator. In der Klasse ist neben ShoppingListAdministrationImpl sämtliche
+ * Applikationslogik vorhanden.
+ * 
+ * @author Luca Randecker
+ * @version 1.0
+ */
+
+@SuppressWarnings("serial")
+public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportGenerator{
+	
+	/**
+	 * Der ReportGenerator benötigt Zugriff auf die ContactAdministation,
+	 * da dort wichtige Methoden für die Koexistenz von Datenobjekten enthalten sind.
+	 */
+    private ShoppingListAdministration administration = null;
+
+	public ReportGeneratorImpl() throws IllegalArgumentException {	
 		
+	}
+
+	@Override
+	public void init() throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ItemsByGroupReport createGroupStatisticsReport(int id) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ShoppingListAdministration getShoppingListAdministration(int id) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemsByPersonReport createUserStatisticsReport(int id) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Item> getAllItems(int id) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Store> getAllStores(int id) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Store getStore(int id) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void AddImprint() throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<ShoppingList> getAllParticipations(Person p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
--- Project_19/src/de/hdm/softwarepraktikum/shared/ReportGeneratorAsync.java
+++ Project_19/src/de/hdm/softwarepraktikum/shared/ReportGeneratorAsync.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import de.hdm.softwarepraktikum.shared.bo.Group;
 import de.hdm.softwarepraktikum.shared.bo.Item;
 import de.hdm.softwarepraktikum.shared.bo.Person;
 import de.hdm.softwarepraktikum.shared.bo.ShoppingList;
 import de.hdm.softwarepraktikum.shared.bo.Store;
+import de.hdm.softwarepraktikum.shared.report.ItemsByGroupReport;
 import de.hdm.softwarepraktikum.shared.report.ItemsByPersonReport;
 
 
@@ -19,6 +21,8 @@ public interface ReportGeneratorAsync {
 	public void getShoppingListAdministration(int id, AsyncCallback<ShoppingListAdministration> callback) throws IllegalArgumentException;
 	
 	public void createUserStatisticsReport(int id, AsyncCallback<ItemsByPersonReport> callback) throws IllegalArgumentException;
+	
+	public void createGroupStatisticsReport(int id, AsyncCallback<ItemsByGroupReport> callback) throws IllegalArgumentException;
  	
 	public void getAllItems(int id, AsyncCallback<ArrayList<Item>> callback) throws IllegalArgumentException;
 	
