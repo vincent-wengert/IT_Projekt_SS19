cbddb41c1526b74f5244c3c5b655e826eba5212e
vincentwengert <vincent.wengert@web.de>
2019-05-20 14:19:05 +0200

    added tabpanel tabs for group,items,stores and shoppinglists

--- Project_19/src/de/hdm/softwarepraktikum/client/gui/NavigatorPanel.java
+++ Project_19/src/de/hdm/softwarepraktikum/client/gui/NavigatorPanel.java
@@ -1,7 +1,41 @@
 package de.hdm.softwarepraktikum.client.gui;
 
 import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class NavigatorPanel extends TabPanel {
+	
 
+private VerticalPanel contentPanelGroups = new VerticalPanel();
+private VerticalPanel contentPanelShoppinglists = new VerticalPanel();
+private VerticalPanel contentPanelStores = new VerticalPanel();
+private VerticalPanel contentPanelArticles = new VerticalPanel();
+		
+/**
+ * ***************************************************************************
+ * ABSCHNITT der Methoden
+ * ***************************************************************************
+ */
+
+
+ /**
+  * In dieser Methode wird das Design des NavigatorPanels und der Widgets festgelegt. 
+  * Diese Methode wird aufgerufen, sobald ein Objekt dieser Klasse instanziert wird. 
+  */
+public void onLoad() {
+
+
+		this.setWidth("35vw");
+		this.setAnimationEnabled(true);
+		
+		
+		
+		this.add(contentPanelGroups, "Gruppen");
+		this.selectTab(0);
+		this.add(contentPanelShoppinglists, "Einkaufslisten");
+		this.add(contentPanelStores, "Alle Händler");
+		this.add(contentPanelArticles, "Alle Artikel");
+		
+	}
 }
+
