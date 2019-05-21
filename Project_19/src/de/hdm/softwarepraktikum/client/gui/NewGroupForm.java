400ea104e268f38c1ccc8ca14f1dc01e52c71a3b
vincentwengert <vincent.wengert@web.de>
2019-05-21 13:05:11 +0200

    added search in newGroupForm, for adding persons by searching names

--- Project_19/src/de/hdm/softwarepraktikum/client/gui/NewGroupForm.java
+++ Project_19/src/de/hdm/softwarepraktikum/client/gui/NewGroupForm.java
@@ -27,6 +27,9 @@ public class NewGroupForm extends VerticalPanel {
 
 	private Label infoTitleLabel = new Label("Neue Gruppe erstellen");
 	private Label groupNameLabel = new Label("Name der Gruppe");
+	private Label groupMembersLabel = new Label("Mitglieder");
+	private Label addedGroupMembersLabel = new Label("Mitglieder der Gruppe");
+
 
 	private TextBox groupNameBox = new TextBox();
 
@@ -79,6 +82,11 @@ public class NewGroupForm extends VerticalPanel {
 		groupGrid.setCellSpacing(10);
 		groupGrid.setWidget(0, 0, groupNameLabel);
 		groupGrid.setWidget(0, 1, groupNameBox);
+		groupGrid.setWidget(1, 0, groupMembersLabel);
+		
+		SearchFormPersons sfp = new SearchFormPersons();
+		groupGrid.setWidget(1, 1, sfp);
+		groupGrid.setWidget(2, 0, addedGroupMembersLabel);
 		
 		this.add(bottomButtonsPanel);
 		this.setCellHorizontalAlignment(bottomButtonsPanel, ALIGN_CENTER);
@@ -123,4 +131,34 @@ public class NewGroupForm extends VerticalPanel {
 		RootPanel.get("Details").clear();
 		}
 	}
+	
+	
+	
+	private class SearchFormPersons extends VerticalPanel {
+
+		private Grid searchGridPersons = new Grid(1, 3);
+
+	
+		private Button addButton = new Button("\u271A");
+		private Button cancelButton = new Button("\u2716");
+		
+		private MultiWordSuggestOracle searchbar = new MultiWordSuggestOracle();
+		private SuggestBox searchTextBox = new SuggestBox(searchbar);
+
+	@SuppressWarnings("deprecation")
+	public void onLoad() {
+	
+		searchTextBox.setSize("300px", "27px");
+		cancelButton.setPixelSize(30, 30);
+		searchTextBox.getElement().setPropertyString("placeholder", "Mitgliedernamen eingeben...");
+		cancelButton.setStylePrimaryName("cancelSearchButton");	
+		addButton.setStylePrimaryName("addPersonButton");
+		
+		searchGridPersons.setWidget(0, 0, searchTextBox);
+		searchGridPersons.setWidget(0, 1, addButton);
+		searchGridPersons.setWidget(0, 2, cancelButton);
+		
+		this.add(searchGridPersons);
+	}
+}
 }
