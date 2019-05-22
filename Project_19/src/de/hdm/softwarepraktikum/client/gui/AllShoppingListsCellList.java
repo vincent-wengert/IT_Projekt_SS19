b3eeb171a5f32591cd14606a53a6b970b24d16e0
JanDuwe <janduwehdm@gmail.com>
2019-05-22 16:56:44 +0200

    celltable for viewing democontents in a list now working

--- Project_19/src/de/hdm/softwarepraktikum/client/gui/AllShoppingListsCellList.java
+++ Project_19/src/de/hdm/softwarepraktikum/client/gui/AllShoppingListsCellList.java
@@ -5,20 +5,23 @@ import java.util.ArrayList;
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
 
-import de.hdm.softwarepraktikum.shared.bo.ShoppingList;
 
 public class AllShoppingListsCellList extends VerticalPanel{
+	private static int itemCounter = 1;
+	private static int listCounter = 1;
 	private ShoppingListKeyProvider keyProvider = null;
 	private CellList<DemoShoppingList> cellList = new CellList<DemoShoppingList>(new ShoppingListCell(), keyProvider);
 	private SingleSelectionModel<DemoShoppingList> selectionModel = null;
 	private ListDataProvider<DemoShoppingList> dataProvider = new ListDataProvider<DemoShoppingList>();
 	private ArrayList<DemoShoppingList> shoppingLists = new ArrayList<DemoShoppingList>();
+	private ShowShoppingListForm sslf;
 	
 	/**
 	 * Erstellen von Demo-Objekten
@@ -27,6 +30,12 @@ public class AllShoppingListsCellList extends VerticalPanel{
 	DemoShoppingList sl1 = new DemoShoppingList("Liste 1");
 	DemoShoppingList sl2 = new DemoShoppingList("Liste 2");		
 	DemoShoppingList sl3 = new DemoShoppingList("Liste 3");
+
+	ItemDemo i1 = new ItemDemo("Apfel", "Kilo", 1);
+	ItemDemo i2 = new ItemDemo("Birne", "Kilo", 2);
+	ItemDemo i3 = new ItemDemo("Zitrone", "Kilo", 1);
+	ItemDemo i4 = new ItemDemo("Wassermelone", "Kilo", 100);
+	ItemDemo i5 = new ItemDemo("Orange", "Kilo", 4);
 	
 	public void onLoad(){
 		getAllShoppingLists();
@@ -45,9 +54,26 @@ public class AllShoppingListsCellList extends VerticalPanel{
 		shoppingLists.add(sl2);
 		shoppingLists.add(sl3);
 		
+		sl1.addToList(i1);
+		sl1.addToList(i2);
+		sl2.addToList(i3);
+		sl3.addToList(i4);
+		sl3.addToList(i5);
+		
 		for (DemoShoppingList sl: shoppingLists) {
 			  dataProvider.getList().add(sl);
 		  }
+	}
+	
+	public void setSelectedShoppingList(DemoShoppingList sl) {
+		//selectedShoppingList = c;
+		
+		RootPanel.get("Details").clear();
+		Notification.show("clear Details");
+		sslf = new ShowShoppingListForm();
+		sslf.setSelected(sl);
+		Notification.show(sl.getName());
+		RootPanel.get("Details").add(sslf);
 	}
 	
 	private class ShoppingListKeyProvider implements ProvidesKey<DemoShoppingList>{
@@ -63,7 +89,7 @@ public class AllShoppingListsCellList extends VerticalPanel{
 		public void onSelectionChange(SelectionChangeEvent event) {
 			DemoShoppingList selection = selectionModel.getSelectedObject();
 			if(selection!=null) {
-			//implement what happens if object gets selected in cell at a later date
+			setSelectedShoppingList(selection);
 			}
 		} 
 	}
@@ -81,25 +107,30 @@ public class AllShoppingListsCellList extends VerticalPanel{
 	 * Demo-Objekt um Liste zu testen
 	 */
 	
-	private class DemoShoppingList{
+	public class DemoShoppingList{
 		//private static int counter = 0;
 		private int id;
 	    private String name;
+	    private ArrayList<ItemDemo> items;
 
 	    
 	    public String getName() {
 			return name;
 		}
 	    
+	    public void addToList(ItemDemo p) {
+	    	this.items.add(p);
+	    }
 	    
 		public void setName(String name) {
 			this.name = name;
 		}
 
 	    public DemoShoppingList(String name) {
-//	    	counter++;
-//	    	this.id = counter;
+	    	listCounter++;
+	    	this.id = listCounter;
 	    	this.name = name;
+	    	items = new ArrayList<ItemDemo>();
 	    }
 
 		@Override
@@ -110,5 +141,59 @@ public class AllShoppingListsCellList extends VerticalPanel{
 		public int getId() {
 			return id;
 		}
+		
+		public ArrayList<ItemDemo> getProductsByList() {
+			return this.items;
+		}
+	}
+	
+	public class ItemDemo {
+			
+		private int id;
+		private String name;
+		private String unit;
+		private int amount;
+	
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getId() {
+			return id;
+		}
+	    
+	    public String getUnit() {
+			return unit;
+		}
+
+		public void setUnit(String unit) {
+			this.unit = unit;
+		}
+
+		public int getAmount() {
+			return amount;
+		}
+
+		public void setAmount(int amount) {
+			this.amount = amount;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public ItemDemo(String name, String unit, int amount) {
+	        itemCounter++;
+	        this.id = itemCounter;
+	        this.name = name;
+	        this.unit = unit;
+	        this.amount = amount;
+	      }
+
 	}
 }
