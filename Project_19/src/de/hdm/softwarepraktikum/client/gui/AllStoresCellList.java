24bbdee26f73b3e0005544510f0fc4c84acdc633
JanDuwe <janduwehdm@gmail.com>
2019-05-23 11:40:20 +0200

    added showform for displaying stores

--- Project_19/src/de/hdm/softwarepraktikum/client/gui/AllStoresCellList.java
+++ Project_19/src/de/hdm/softwarepraktikum/client/gui/AllStoresCellList.java
@@ -6,12 +6,12 @@ import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.cell.client.Cell.Context;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
-
 
 
 public class AllStoresCellList extends VerticalPanel{
@@ -21,11 +21,12 @@ public class AllStoresCellList extends VerticalPanel{
 	private SingleSelectionModel<DemoStore> selectionModel = null;
 	private ListDataProvider<DemoStore> dataProvider = new ListDataProvider<DemoStore>();
 	private ArrayList<DemoStore> stores = new ArrayList<DemoStore>();
+	private StoreForm sf = null;
 
-	DemoStore s1 = new DemoStore("Lidl");
-	DemoStore s2 = new DemoStore("Penny");
-	DemoStore s3 = new DemoStore("Rewe");
-	DemoStore s4 = new DemoStore("Edeka");
+	DemoStore s1 = new DemoStore("Lidl", 70597, "Stuttgart", "Coole Straﬂe", 31);
+	DemoStore s2 = new DemoStore("Penny", 12345, "Berlin", "Uncoole Straﬂe", 32);
+	DemoStore s3 = new DemoStore("Rewe", 40997, "Hannover", "Andere Straﬂe", 33);
+	DemoStore s4 = new DemoStore("Edeka", 69420, "Amsterdam", "van Gogh Straﬂe", 34);
 	
 	public void onLoad() {
 		getAllStores();
@@ -37,6 +38,15 @@ public class AllStoresCellList extends VerticalPanel{
 		cellList.setRowCount(stores.size(), true);
 		cellList.setRowData(0, dataProvider.getList());
 		this.add(cellList);
+	}
+	
+	public void setSelectedStore(DemoStore s) {
+		//selectedShoppingList = c;
+		RootPanel.get("Details").clear();
+		Notification.show("clear Details");
+		sf = new StoreForm();
+		sf.setSelected(s);
+		RootPanel.get("Details").add(sf);
 	}
 	
 	public void getAllStores() {
@@ -63,7 +73,7 @@ public class AllStoresCellList extends VerticalPanel{
 		public void onSelectionChange(SelectionChangeEvent event) {
 			DemoStore selection = selectionModel.getSelectedObject();
 			if(selection!=null) {
-			//implement what happens if object gets selected in cell at a later date
+				setSelectedStore(selection);
 			}
 		} 
 	}
@@ -77,11 +87,62 @@ public class AllStoresCellList extends VerticalPanel{
 	    }
 	}
 	
-	private class DemoStore{
+	public class DemoStore{
 		//private static int counter = 0;
 		private int id;
 	    private String name;
+	    private int postLeitZahl;
+		private String ort;
+		private String strasse;
+		private int hausNummer;
+		
+		public DemoStore(String name, int plz, String ort, String strasse, int hausNr) {
+			this.name = name;
+			this.postLeitZahl = plz;
+			this.ort = ort;
+			this.strasse = strasse;
+			this.hausNummer = hausNr;
+			
+		}
+		
+		public int getPostleitZahl() {
+			return postLeitZahl;
+		}
 
+
+		public void setPostleitZahl(int postleitZahl) {
+			this.postLeitZahl = postleitZahl;
+		}
+
+
+		public String getOrt() {
+			return ort;
+		}
+
+
+		public void setOrt(String ort) {
+			this.ort = ort;
+		}
+
+
+		public String getStrasse() {
+			return strasse;
+		}
+
+
+		public void setStrasse(String strasse) {
+			this.strasse = strasse;
+		}
+
+
+		public int getHausNummer() {
+			return hausNummer;
+		}
+
+
+		public void setHausNummer(int hausNummer) {
+			this.hausNummer = hausNummer;
+		}
 	    
 	    public String getName() {
 			return name;
