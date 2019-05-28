485af81acf68282c01521f661c35db09f9027545
vincentwengert <vincent.wengert@web.de>
2019-05-28 15:44:00 +0200

    added commonsettings constructor

--- Project_19/src/de/hdm/softwarepraktikum/shared/CommonSettings.java
+++ Project_19/src/de/hdm/softwarepraktikum/shared/CommonSettings.java
@@ -1,5 +1,20 @@
 package de.hdm.softwarepraktikum.shared;
 
+/**
+ * Zentrale Konstante, die sowohl Server- als auch Client-seitig benötigt
+ * werden. Superklasse von <code>ClientsideSettings</code> und
+ * <code>ServersideSettings</code>.
+ * 
+ * @author VincentWengert
+ * @version 1.0
+ */
+
+
 public class CommonSettings {
 
+    /**
+     * Default constructor
+     */
+    public CommonSettings() {
+    }
 }
